72dae3440ef3dfbf910a3675c26718d478d3c604
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2025-12-11 10:56:59 +0600

    Update practice.java

--- week_9/thread-safety-and-concurreny/practice.java
+++ week_9/thread-safety-and-concurreny/practice.java
@@ -1,11 +1,15 @@
 package thread_safety_and_concurrency;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         /*concurrency vs parallelism: interview question
         */
 
-        // race condition
+        /*// race condition
         Counter counter = new Counter();
 
         Runnable counterTask = () -> { // thread with anonymous class
@@ -30,10 +34,53 @@ public class Main {
 
         // atomic?
 
-        // interview question: optimistic locking and pessimistic locking
+        // interview question: optimistic locking and pessimistic locking*/
 
+        /*// Executor Framework
+        Executor executor = Executors.newFixedThreadPool(2);
+        executor.execute(() -> System.out.println("Task executed")); // gave a runnable*/
+
+        // Fixed threadpool
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for(int i = 1; i <= 5; i++) {
+            int taskId = i;
+            executor.execute(() -> {
+                System.out.println("Task " + taskId + " is running by " + Thread.currentThread().getName());
+            });
+        }
+
+        executor.shutdown(); // try with resource likhle shutdown likhte hobe nah
+
+        // Virtual Threads, need more understanding
+
+        // Creating java virtual thread
+        Runnable runnable = () -> {
+            for(int i=0; i<10; i++) {
+                System.out.println("Index: " + i);
+            }
+        };
+
+        Thread vThread = Thread.ofVirtual().start(runnable);
+        vThread.join();
+        /*If you do not want the virtual thread to start immediately, you can use the unstarted() method instead. Here is an example of
+creating an unstarted virtual thread:*/
+
+        Thread vThreadUnstarted = Thread.ofVirtual().unstarted(runnable);
+        vThreadUnstarted.start();
+
+        /*ExecutorService Using Virtual Threads
+        It is possible to create a Java ExecutorService that uses virtual threads internally. Example:*/
+
+        try (ExecutorService executor2 = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 1; i <= 5; i++) {
+                int taskId = i;
+                executor2.submit(() -> System.out.println("Task " + taskId + " running in " + Thread.currentThread()));
+            }
+        }
     }
 }
+
 
 
 package thread_safety_and_concurrency;
