d85bc8220a8da42f0a37d2b0f5aeb4d6d035792b
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2025-12-16 19:43:15 +0600

    Create practice.java

---
+++ week-10/intro_to_spring_boot/practice.java
@@ -0,0 +1,17 @@
+package introduction_to_spring_core;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        // Ends: 1.13 hr
+        unfamiliar terms: classpath, boilerplate, jar or jars, opinionated defaults, microservice-friendly, spring cloud
+
+        spring initializer: https://start.spring.io/
+        spring web dependencies lagbe rest api bananer jonno
+        */
+
+        // to run via terminal: ./gradlew bootRun
+        // ctrl + c to stop
+        // clear: to clear
+    }
+}
